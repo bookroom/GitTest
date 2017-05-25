d8f8a470a8f53978acfac6e541d45fef5339984a
houhonglei <houhonglei@bbktel.com>
2017-05-25 16:37:54 +0800

    初次修改

--- app/src/main/java/test/git/com/gittest/GitActivity.java
+++ app/src/main/java/test/git/com/gittest/GitActivity.java
@@ -2,12 +2,19 @@ package test.git.com.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GitActivity extends AppCompatActivity {
+
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_git);
+        setContentView(R.layout.gitlayout);
+
+        tv1= (TextView) findViewById(R.id.tv1);
+
+
     }
 }
