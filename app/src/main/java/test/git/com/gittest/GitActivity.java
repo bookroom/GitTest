1ecbec0a0b961b1754b7d6caabef774b8676f335
houhonglei <houhonglei@bbktel.com>
2017-05-26 14:40:58 +0800

    git1

--- app/src/main/java/test/git/com/gittest/GitActivity.java
+++ app/src/main/java/test/git/com/gittest/GitActivity.java
@@ -1,12 +1,15 @@
 package test.git.com.gittest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class GitActivity extends AppCompatActivity {
 
     TextView tv1;
+    Button bn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,13 @@ public class GitActivity extends AppCompatActivity {
         setContentView(R.layout.gitlayout);
 
         tv1= (TextView) findViewById(R.id.tv1);
+        bn= (Button) findViewById(R.id.changetext);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bn.setText("张家界");
+            }
+        });
 
 
     }
